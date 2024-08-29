cee86fd3183ef13cb2c15a35ce43df9ead3bddb1
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-08-29 17:36:47 +0530

    Removed Cors

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const app = express();
 const port = process.env.PORT;
 const databaseURL = process.env.DATABSE_URL;
 
-app.use(
-  cors({
-    origin: [process.env.ORIGIN],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.ORIGIN],
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/uploads/profiles", express.static("uploads/profiles"));
 app.use("/uploads/files", express.static("uploads/files"));
